ba3bb25f0e247d811757cef2e8f073edb7b18469
arttschie <101570095+arttschie@users.noreply.github.com>
2022-11-02 09:39:48 +0600

    Update NewsService.java

--- src/main/java/org/nogin/service/NewsService.java
+++ src/main/java/org/nogin/service/NewsService.java
@@ -8,7 +8,10 @@ import java.util.List;
 //TODO CRUD
 public interface NewsService {
     List<News> getNews();
+    List<News> getNewsByUser(User user);
     News findById(Long id);
     News findByTitle(String title);
     void createNews(User user);
+    News updateNewsTitle(News news, User user);
+    News updateNewsContent(News news, User user);
 }
