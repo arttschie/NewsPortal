b8f66bd933e626b63dcefde6943a930b225ecde6
arttschie <101570095+arttschie@users.noreply.github.com>
2022-11-02 09:56:14 +0600

    Update NewsRepository.java

--- src/main/java/org/nogin/repository/NewsRepository.java
+++ src/main/java/org/nogin/repository/NewsRepository.java
@@ -1,12 +1,17 @@
 package org.nogin.repository;
 
 import org.nogin.entity.News;
+import org.nogin.entity.User;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface NewsRepository {
     List<News> findAll();
+    List<News> findAllByUser(User user);
     News findById(Long id);
     Optional<News> findByTitle(String title);
+    void insertNews(User user);
+    News updateNewsTitle(News news, User user);
+    News updateNewsContent(News news, User user);
 }
