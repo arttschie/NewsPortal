ee9dde052ab14175472deac9a1b60ac1c4967f39
arttschie <101570095+arttschie@users.noreply.github.com>
2022-11-02 13:43:10 +0600

    Create UserRepository.java

---
+++ src/main/java/org/nogin/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package org.nogin.repository;
+
+import org.nogin.entity.News;
+import org.nogin.entity.User;
+
+import java.util.List;
+
+public interface UserRepository {
+    List<User> findAll();
+    User findUserByNews(News news);
+    User findById(Long id);
+    User findByLogin(String login);
+    User findByPassword(String password);
+    void insertUser();
+    User updateUserLogin(User user);
+    User updateUserPassword(User user);
+}
