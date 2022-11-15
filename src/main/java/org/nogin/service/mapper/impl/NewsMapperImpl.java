4081e4cb32be8f473f22a9852daa390bd66ec572
arttschie <101570095+arttschie@users.noreply.github.com>
2022-11-15 10:53:45 +0600

    Update NewsMapperImpl.java

--- src/main/java/org/nogin/service/mapper/impl/NewsMapperImpl.java
+++ src/main/java/org/nogin/service/mapper/impl/NewsMapperImpl.java
@@ -4,8 +4,8 @@ import org.nogin.service.mapper.NewsMapper;
 import org.nogin.service.models.News;
 import org.nogin.service.models.User;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class NewsMapperImpl implements NewsMapper {
     @Override
@@ -34,51 +34,35 @@ public class NewsMapperImpl implements NewsMapper {
 
     @Override
     public org.nogin.database.entity.News mapToDatabase(News source) {
+        if (source == null) {
+            return null;
+        }
+
+        org.nogin.database.entity.User user = null;
+        
+        if (source.getUser() != null) {
+            user = org.nogin.database.entity.User.builder()
+                    .id(source.getUser().getId())
+                    .login(source.getUser().getLogin())
+                    .password(source.getUser().getPassword())
+                    .build();
+        }
+        
         return org.nogin.database.entity.News.builder()
                 .id(source.getId())
                 .title(source.getTitle())
                 .content(source.getContent())
-                .user(org.nogin.database.entity.User.builder()
-                    .id(source.getUser().getId())
-                    .login(source.getUser().getLogin())
-                    .password(source.getUser().getPassword())
-                    .build())
+                .user(user)
                 .build();
     }
 
     @Override
     public List<News> mapToService(List<org.nogin.database.entity.News> source) {
-        List<News> listNewsService = new ArrayList<>();
-        for (org.nogin.database.entity.News news: source) {
-            listNewsService.add(News.builder()
-                                    .id(news.getId())
-                                    .title(news.getTitle())
-                                    .content(news.getContent())
-                                    .user(User.builder()
-                                        .id(news.getUser().getId())
-                                        .login(news.getUser().getLogin())
-                                        .password(news.getUser().getPassword())
-                                        .build())
-                                    .build());
-        }
-        return listNewsService;
+        return source.stream().map(this::mapToService).collect(Collectors.toList());
     }
 
     @Override
     public List<org.nogin.database.entity.News> mapToDatabase(List<News> source) {
-        List<org.nogin.database.entity.News> listNewsDatabase = new ArrayList<>();
-        for (News news : source) {
-            listNewsDatabase.add(org.nogin.database.entity.News.builder()
-                                        .id(news.getId())
-                                        .title(news.getTitle())
-                                        .content(news.getContent())
-                                        .user(org.nogin.database.entity.User.builder()
-                                            .id(news.getUser().getId())
-                                            .login(news.getUser().getLogin())
-                                            .password(news.getUser().getPassword())
-                                            .build())
-                                        .build());
-        }
-        return listNewsDatabase;
+        return source.stream().map(this::mapToDatabase).collect(Collectors.toList());
     }
 }
