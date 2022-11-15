dee961c87c61eabd9378e812bd642ca2f0964d03
arttschie <101570095+arttschie@users.noreply.github.com>
2022-11-15 10:31:22 +0600

    Update UserServiceImpl.java

--- src/main/java/org/nogin/service/impl/UserServiceImpl.java
+++ src/main/java/org/nogin/service/impl/UserServiceImpl.java
@@ -1,84 +1,62 @@
 package org.nogin.service.impl;
 
-import org.nogin.service.mapper.NewsMapper;
-import org.nogin.service.mapper.UserMapper;
-import org.nogin.service.models.News;
-import org.nogin.service.models.User;
 import org.nogin.database.repository.UserRepository;
 import org.nogin.service.UserService;
-import org.springframework.stereotype.Service;
+import org.nogin.service.mapper.UserMapper;
+import org.nogin.service.models.User;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
-@Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
-    private final NewsMapper newsMapper;
 
-    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper, NewsMapper newsMapper) {
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
-        this.newsMapper = newsMapper;
     }
 
     @Override
     public List<User> getUsers() {
-        return this.userRepository.findAll().stream()
+        return userRepository.findAll().stream()
                 .map(userMapper::mapToService)
                 .collect(Collectors.toList());
     }
-    
+
     @Override
-    public User getUserByNews(News news) {
-        org.nogin.database.entity.News newsEntity = newsMapper.mapToDatabase(news);
-        org.nogin.database.entity.User userEntity = userRepository.findUserByNewsId(newsEntity);
-        User user = userMapper.mapToService(userEntity);
-        return user;
+    public Optional<User> getUserByNewsId(Long newsId) {
+        return userRepository.findUserByNewsId(newsId).map(userMapper::mapToService);
     }
 
     @Override
-    public User getById(Long id) {
-        org.nogin.database.entity.User entity = userRepository.findById(id);
-        User user = userMapper.mapToService(entity);
-        return user;
+    public Optional<User> getById(Long id) {
+        return userRepository.findById(id).map(userMapper::mapToService);
     }
-    
-    @Override
-    public User getByLogin(String login) {
-        org.nogin.database.entity.User entity = userRepository.findByLogin(login);
-        User user = userMapper.mapToService(entity);
-        return user;
-    }
-    
-    @Override
-    public User getByPassword(String password) {
-        org.nogin.database.entity.User entity = userRepository.findByPassword(password);
-        User user = userMapper.mapToService(entity);
-        return user;
-    }
-    
-    @Override
-    public void createUser() {
 
-    }
-    
     @Override
-    public User changeUserLogin(User user, String login) {
-        return User.builder()
-                  .id(user.getId())
-                  .login(login)
-                  .password(user.getPassword())
-                .build();
+    public Optional<User> getByLogin(String login) {
+        return userRepository.findByLogin(login).map(userMapper::mapToService);
     }
-    
+
     @Override
-    public User changeUserPassword(User user, String password) {
-        return User.builder()
-                  .id(user.getId())
-                  .login(user.getLogin())
-                  .password(password)
-                .build();
+    public Optional<User> getByPassword(String password) {
+        return userRepository.findByPassword(password).map(userMapper::mapToService);
+    }
+
+    @Override
+    public void createUser(User user) {
+        userRepository.createUser(userMapper.mapToDatabase(user));
+    }
+
+    @Override
+    public void changeUserLogin(Long userId, String login) {
+        userRepository.updateUserLogin(userId, login);
+    }
+
+    @Override
+    public void changeUserPassword(Long userId, String password) {
+        userRepository.updateUserPassword(userId, password);
     }
 }
