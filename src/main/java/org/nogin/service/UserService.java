638f89c438ca4016db6af8dd2a12780cb368e238
arttschie <101570095+arttschie@users.noreply.github.com>
2022-11-15 10:30:29 +0600

    Update UserService.java

--- src/main/java/org/nogin/service/UserService.java
+++ src/main/java/org/nogin/service/UserService.java
@@ -1,17 +1,18 @@
 package org.nogin.service;
 
-import org.nogin.service.models.News;
 import org.nogin.service.models.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserService {
-  List<User> getUsers();
-  User getUserByNews(News news);
-  User getById(Long id);
-  User getByLogin(String login);
-  User getByPassword(String password);
-  void createUser();
-  User changeUserLogin(User user, String login);
-  User changeUserPassword(User user, String password);
+    List<User> getUsers();
+    Optional<User> getUserByNewsId(Long newsId);
+    Optional<User> getById(Long id);
+    Optional<User> getByLogin(String login);
+    Optional<User> getByPassword(String password);
+    void createUser(User user);
+    void changeUserLogin(Long userId, String login);
+    void changeUserPassword(Long userId, String password);
 }
+
