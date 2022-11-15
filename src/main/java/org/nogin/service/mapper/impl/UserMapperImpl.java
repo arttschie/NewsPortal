895202abd7570f09297b551bd9e674a0f4e809da
arttschie <101570095+arttschie@users.noreply.github.com>
2022-11-15 11:04:38 +0600

    Update UserMapperImpl.java

--- src/main/java/org/nogin/service/mapper/impl/UserMapperImpl.java
+++ src/main/java/org/nogin/service/mapper/impl/UserMapperImpl.java
@@ -4,26 +4,43 @@ import org.nogin.service.mapper.UserMapper;
 import org.nogin.service.models.User;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserMapperImpl implements UserMapper {
 
     @Override
     public User mapToService(org.nogin.database.entity.User source) {
-        return null;
+        if (source == null) {
+            return null;
+        }
+
+        return User.builder()
+                .id(source.getId())
+                .login(source.getLogin())
+                .password(source.getPassword())
+                .build();
     }
 
     @Override
     public org.nogin.database.entity.User mapToDatabase(User source) {
-        return null;
+        if (source == null) {
+            return null;
+        }
+        
+        return org.nogin.database.entity.User.builder()
+                .id(source.getId())
+                .login(source.getLogin())
+                .password(source.getPassword())
+                .build();
     }
 
     @Override
     public List<User> mapToService(List<org.nogin.database.entity.User> source) {
-        return null;
+        return source.stream().map(this::mapToService).collect(Collectors.toList());
     }
 
     @Override
     public List<org.nogin.database.entity.User> mapToDatabase(List<User> source) {
-        return null;
+        return source.stream().map(this::mapToDatabase).collect(Collectors.toList());
     }
 }
