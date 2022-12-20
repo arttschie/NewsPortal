aeadf1afbcdb962fdcb5a7d61adf051c729d5156
arttschie <101570095+arttschie@users.noreply.github.com>
2022-12-20 18:18:50 +0600

    Update NewsPortalIntegrationTest.java

--- src/test/java/org/nogin/newsportal/NewsPortalIntegrationTest.java
+++ src/test/java/org/nogin/newsportal/NewsPortalIntegrationTest.java
@@ -1,25 +1,28 @@
 package org.nogin.newsportal;
 
 import org.hibernate.SessionFactory;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.hibernate.cfg.Configuration;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.nogin.newsportal.database.repository.UserRepository;
 import org.nogin.newsportal.database.repository.impl.UserRepositoryImpl;
-import org.nogin.newsportal.database.util.HibernateUtil;
 import org.nogin.newsportal.service.UserService;
 import org.nogin.newsportal.service.impl.UserServiceImpl;
 import org.nogin.newsportal.service.mapper.UserMapper;
 import org.nogin.newsportal.service.mapper.impl.UserMapperImpl;
+import org.nogin.newsportal.service.models.User;
 import org.testcontainers.containers.OracleContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.Properties;
+
 @Testcontainers
-@ExtendWith(MockitoExtension.class)
 public class NewsPortalIntegrationTest {
     @Container
     private static final OracleContainer oracle = new OracleContainer(DockerImageName.parse("gvenzl/oracle-xe"));
@@ -28,32 +31,44 @@ public class NewsPortalIntegrationTest {
     private static UserRepository userRepository;
     private static UserMapper userMapper;
     private static SessionFactory sessionFactory;
+    private static Properties connectionProps;
+
+    private User user;
 
     @BeforeAll
     public static void startUp() {
-        System.out.println(oracle.getPassword());
-        System.out.println(oracle.getUsername());
-        System.out.println(oracle.getJdbcUrl());
-//        oracle.start();
-        sessionFactory = HibernateUtil.getSessionFactory();
+        oracle.start();
+        connectionProps = new Properties();
+        connectionProps.put("hibernate.connection.driver_class", "oracle.jdbc.OracleDriver");
+        connectionProps.put("hibernate.connection.url", oracle.getJdbcUrl());
+        connectionProps.put("hibernate.connection.username", oracle.getUsername());
+        connectionProps.put("hibernate.connection.password", oracle.getPassword());
+        connectionProps.put("hibernate.dialect", "org.hibernate.dialect.Oracle8iDialect");
+        connectionProps.put("show_sql", true);
+        connectionProps.put("hibernate.hbm2ddl.auto", "create-drop");
+        sessionFactory = new Configuration().addProperties(connectionProps).buildSessionFactory();
         userMapper = new UserMapperImpl();
         userRepository = new UserRepositoryImpl(sessionFactory);
         userService = new UserServiceImpl(userRepository, userMapper);
     }
 
+    @BeforeEach
+    public void beforeEachTest() {
+        user = User.builder()
+                .id(0L)
+                .login("Test login")
+                .password("Test password")
+                .build();
+    }
+
     @Test
     public void createUserTest() {
-        System.out.println(oracle.getPassword());
-        System.out.println(oracle.getUsername());
-        System.out.println(oracle.getJdbcUrl());
-//        userService.createUser(User.builder()
-//                .id(0L)
-//                .login("Test login")
-//                .password("Test password")
-//                .build());
-//
-//        User user = userService.getById(0L).get();
-//        System.out.println(user);
+        userService.createUser(user);
+//        assertThat(userService.getUsers()).isEqualTo(Arrays.asList(user));
+//        assertThat(userService.getById(0L)).isEqualTo(user);
+//        assertThat(userService.getByLogin("Test login")).isEqualTo(user);
+//        assertThat(userService.getByPassword("Test login")).isEqualTo(user);
+//        System.out.println(sessionFactory);
     }
 
     @AfterAll
